b69bab8d7b1bf2262f3701ced53be0a4fa869fed
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2020-10-13 12:33:07 +0100

    Add placeholder for more tests

---
+++ src/Sempare.Streams.List.Test.pas
@@ -0,0 +1,81 @@
+(*%****************************************************************************
+ *                 ___                                                        *
+ *                / __|  ___   _ __    _ __   __ _   _ _   ___                *
+ *                \__ \ / -_) | '  \  | '_ \ / _` | | '_| / -_)               *
+ *                |___/ \___| |_|_|_| | .__/ \__,_| |_|   \___|               *
+ *                                    |_|                                     *
+ ******************************************************************************
+ *                                                                            *
+ *                        Sempare Streams                                     *
+ *                                                                            *
+ *                                                                            *
+ *          https://www.github.com/sempare/sempare-streams                    *
+ ******************************************************************************
+ *                                                                            *
+ * Copyright (c) 2020 Sempare Limited,                                        *
+ *                    Conrad Vermeulen <conrad.vermeulen@gmail.com>           *
+ *                                                                            *
+ * Contact: info@sempare.ltd                                                  *
+ *                                                                            *
+ * Licensed under the GPL Version 3.0 or the Sempare Commercial License       *
+ * You may not use this file except in compliance with one of these Licenses. *
+ * You may obtain a copy of the Licenses at                                   *
+ *                                                                            *
+ * https://www.gnu.org/licenses/gpl-3.0.en.html                               *
+ * https://github.com/sempare/sempare-streams/tree/dev/docs/commercial.license.md *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the Licenses is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ *                                                                            *
+ ****************************************************************************%*)
+unit Sempare.Streams.List.Test;
+
+interface
+
+uses
+  System.Generics.Collections,
+  System.SysUtils,
+  Sempare.Streams.Test.Common,
+  DUnitX.TestFramework;
+
+type
+
+  [TestFixture]
+  TStreamListTest = class(TStreamsTestBase)
+  public
+    [Setup]
+    procedure Setup; override;
+
+    [Teardown]
+    procedure Teardown; override;
+
+  end;
+
+implementation
+
+uses
+  Spring.Collections,
+  Sempare.Streams.Spring4d;
+
+{ TStreamListTest }
+
+procedure TStreamListTest.Setup;
+begin
+  inherited;
+
+end;
+
+procedure TStreamListTest.Teardown;
+begin
+  inherited;
+
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TStreamListTest);
+
+end.
