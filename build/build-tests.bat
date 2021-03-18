952e986d98962c480e2ce4122a98b9149e2cc12d
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2021-03-18 10:23:28 +0000

    Remove build bat

--- build/build-tests.bat
+++
@@ -1,3 +0,0 @@
-cd %MSBUILD_TARGET_DIR%
-msbuild %MSBUILD_TARGET% /t:Build /p:Configuration=%BUILD_CONFIG% /p:platform=%BUILD_PLATFORM%
-%BUILDTOOLS_WIN32%\GitHubStatusNotifier.exe -gitsha:%BUILD_VCS_NUMBER% -successif:%ERRORLEVEL% -context:%BUILD_PLATFORM%,%BUILD_CONFIG% -targeturl:%DUNITX_TARGET_URL%
