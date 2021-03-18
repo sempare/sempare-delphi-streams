b24468c6d8a4e3f165bc49d0643f904519245a64
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2021-03-18 10:08:42 +0000

    Add build batch for CI

---
+++ build/build-tests.bat
@@ -0,0 +1,2 @@
+msbuild %MSBUILD_TARGET% /t:Build /p:Configuration=%BUILD_CONFIG% /p:platform=%BUILD_PLATFORM%
+%BUILDTOOLS_WIN32%\GitHubStatusNotifier.exe -gitsha:%BUILD_VCS_NUMBER% -successif:%ERRORLEVEL% -context:%BUILD_PLATFORM%,%BUILD_CONFIG% -targeturl:%DUNITX_TARGET_URL%
